d1c581a2f1afa368c611bd140e0e781f323800fe
kolya944994 <kolya944.994@mail.ru>
2025-09-21 22:07:45 +0500

    dfd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import MainPage from './pages/mainPage/MainPage'
-import VacancyPage from './pages/vacancyPage/VacancyPage'
 
 function App() {
 	return (
 		<>
 			<MainPage></MainPage>
-			<VacancyPage></VacancyPage>
 		</>
 	)
 }
